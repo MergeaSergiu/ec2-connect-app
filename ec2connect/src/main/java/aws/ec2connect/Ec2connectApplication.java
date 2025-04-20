720142fb7404b1312700f3a4ea74cad940a81cef
MergeaSergiu <mergeasergiu@gmail.com>
2025-04-20 20:25:44 +0300

    first commit

---
+++ ec2connect/src/main/java/aws/ec2connect/Ec2connectApplication.java
@@ -0,0 +1,13 @@
+package aws.ec2connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2connectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2connectApplication.class, args);
+	}
+
+}
---
+++ ec2connect/src/test/java/aws/ec2connect/Ec2connectApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.ec2connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2connectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
